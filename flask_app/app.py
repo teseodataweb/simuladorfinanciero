9a52477f80e811818dc20028d22f2c9c34ed1ece
teseodataweb <teseo.landing@gmail.com>
2025-06-29 11:37:20 -0600

    Integrar frontend como template y static en Flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,26 +1,23 @@
-import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from flask_app.modelo_riesgo_credito import simular_credito
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
 @app.route('/')
 def home():
-    return "App Flask en Render funcionando ✅"
+    return render_template("index.html")
 
 @app.route('/simular', methods=['POST'])
 def simular():
     try:
         datos = request.get_json()
         resultados = simular_credito(datos)
+
         if 'tabla_amortizacion' in resultados:
             resultados['tabla_amortizacion'] = resultados['tabla_amortizacion'].to_dict(orient='records')
+
         return jsonify(resultados)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
